bb0858b125bb6f4eb1551136b1dcdafd0cd14655
whindes <bhindes@hotmail.com>
2017-12-12 10:09:06 -0800

    Run script in Windows 7 environments may need a little workaround. Tightened up spacess in entrypoint and update credit in Readme.

--- run.sh
+++ run.sh
@@ -5,10 +5,11 @@
 NAME="jmeter"
 IMAGE="whindes/jmeter:3.3"
 
-# Use the current working dir
-WORK_DIR="`pwd`"
-
+# Use the current working directory
+# in Linux (Some struggles in Windows 7 using Virtualbox *Check shared folder and you may have to use PowerShell to run the last line.)
+WORK_DIR="/tmp/jmeter"
+LOCAL_DIR="`pwd`"
 # Finally run
 docker stop ${NAME} > /dev/null 2>&1
 docker rm ${NAME} > /dev/null 2>&1
-docker run --name ${NAME} -i -v ${WORK_DIR}:${WORK_DIR} -w ${WORK_DIR} ${IMAGE} $@
+docker run --rm --name ${NAME} -i -v ${LOCAL_DIR}:${WORK_DIR} -w ${WORK_DIR} ${IMAGE} $@
